7d09506c05b9b710a1ef652ba7ffd7dc26c70915
Naresh <nareshkumar504@gmail.com>
2020-03-15 15:26:22 +0530

    adding account class

---
+++ src/com/grms/java14/features/domain/Account.java
@@ -0,0 +1,25 @@
+package com.grms.java14.features.domain;
+
+/**
+ * Created by grmsnaresh on 15-03-2020.
+ */
+public class Account {
+    long id;
+    String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
